c6b8a3463b752183fd10125bb3c99e1856ba063b
Alexander Tretyakov <tpetyakov@gmail.com>
2022-05-18 13:55:10 +0300

    fix: 04

--- 04-copy-directory/utils.js
+++ 04-copy-directory/utils.js
@@ -1,18 +1,22 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+async function copy(src, dest) {
+  await fs.mkdir(dest);
+  const files = await fs.readdir(src, { withFileTypes: true });
+  await Promise.all(files.map(async (file) => {
+    const srcFilePath = path.join(src, file.name);
+    const destFilePath = path.join(dest, file.name);
+    return file.isDirectory()
+      ? copy(srcFilePath, destFilePath)
+      : fs.copyFile(srcFilePath, destFilePath);
+  }));
+}
+
 async function copyDirectory(source, destination) {
   try {
     await fs.rm(destination, { recursive: true, force: true });
-    await fs.mkdir(destination);
-    const files = await fs.readdir(source, { withFileTypes: true });
-    await Promise.all(files.map(async (file) => {
-      const srcFilePath = path.join(source, file.name);
-      const destFilePath = path.join(destination, file.name);
-      return file.isDirectory()
-        ? copyDirectory(srcFilePath, destFilePath)
-        : fs.copyFile(srcFilePath, destFilePath);
-    }));
+    await copy(source, destination);
   } catch (error) {
     console.error(error);
   }
